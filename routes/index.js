35b845a7f245c973c20fd64469eb57f75071e8ce
Kofi Ramos Amoussou <karsoft92@gmail.com>
2019-06-17 07:12:33 +0000

    feat: added sequelize package

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let landing = require('../controllers/landing.js');
 /* GET home page. */
-router.get('/',landing.index);
+router.get('/',landing.get_landing);
+router.post('/', landing.submit_lead);
 
 module.exports = router;
